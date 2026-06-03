57c389a0da44e755b1c7200d95102190a46d1a57
Sparsh-25 <wadhwasparsh25@gmail.com>
2026-06-03 18:23:04 +0530

    Extracted from PDFS v1

--- Extraction.py
+++ Extraction.py
@@ -1,12 +1,31 @@
-from pypdf import PdfReader
+import pymupdf
+from pathlib import Path
+import time
 
-reader = PdfReader("data/NCERT-Class-12-Physics-Part-2.pdf")
-print(len(reader.pages))
+data_dir = Path("data")
 
-# meta = reader.metadata
+output_dir = Path("output")
 
-# print(meta)
-# print(meta.author)
-# print(meta.creator)
+output_dir.mkdir(exist_ok=True)
 
-print(reader.pages[56].extract_text())
+for pdf_path in data_dir.glob("*.pdf"):
+
+    print("Processing", pdf_path.name)
+
+    doc = pymupdf.open(pdf_path)
+
+    start = time.time()
+
+    full_text = []
+
+    for i, page in enumerate(doc):
+        text = page.get_text()  
+        full_text.append(text)
+
+        print(f"Done with page {i+1}/{len(doc)}")
+        print(f"Pages left: {len(doc) - i - 1}")
+        print(f"Time elapsed: {time.time()-start:.1f}s\n")
+
+    out_file = output_dir / f"{pdf_path.stem}.txt"
+    out_file.write_text("\n\n".join(full_text))
+    print(f"Saved → {out_file}\n")
